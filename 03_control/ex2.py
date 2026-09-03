ee3b563d68a0342b5b42a90ec3371c76c250f81c
yunwooSong <russy10370@gmail.com>
2026-09-03 15:29:47 +0900

    9/3 (ex1.py,ex2.py,ex3.py)

---
+++ 03_control/ex2.py
@@ -0,0 +1,41 @@
+﻿# 반복문 : while, for문
+
+# while
+# 1~10까지 반복 출력
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+    if i == 5:
+        break
+else:
+    print('end')
+
+nums = [1, 3, 5, 7, 9]
+target = 2
+# found = False
+
+i = 0
+while i < len(nums):
+    if target == nums[i]:
+        print(f'{target} found')
+        # found = True
+        break
+    i += 1
+else:
+    print(f'{target} not found')
+
+# if not found:
+#   print(f'{target} not found')
+
+# 1~10까지의 합
+i = 1
+tot = 0
+
+while i <= 10:
+    i += 1
+    if i % 2 == 1:
+        continue
+    tot += i
+else:
+    print(tot)
---
+++ 03_control/ex3.py
@@ -0,0 +1,49 @@
+﻿# for문
+
+# for(int i=0;i<=10;i++)
+# for i in iterable 객체:
+
+# 0 ~4
+for i in range(5):
+    print(i, end = "")
+print()
+
+a = range(5)
+print(a.start, a.stop, a.step)
+
+# 1~5
+for i in range(1, 6):
+    print(i, end = "")
+print()
+
+# 0 ~10 짝수
+for i in range(0, 11, 2):
+    print(i, end = "")
+print()
+
+# 5,4,3,2,1
+for i in range(5, 0, -1):
+    print(i, end = "")
+print()
+
+# 1~10까지의 합
+tot = 0
+for i in range(1, 10):
+    tot += i
+print(tot)
+
+print(sum(range(1, 11)))
+
+s = "hi12!@한글人📜"
+for c in s:
+    print(c, end = " ")
+print()
+print(len(s))
+
+# 구구단 출력
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(f"{i:2d} x {j:2d} = {i * j:2d}", end = "\t")
+    print()
+else:
+    print('end')
